84dd605da60136c165218d2713402a7068de7a89
Moïse <moise.yombo@edu.univ-fcomte.fr>
2020-09-30 16:09:06 +0200

    media query tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,9 @@ module.exports = {
       'sans': ['Permanent Marker'],
     },
     extend: {
+      screens: {
+        'portrait': {'raw': '(orientation: portrait)'},
+      },
       colors:{
         'regal-grey': 'grey',
       }
